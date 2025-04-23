b7c67dc30ef5919b30688034a3feacdb360051ba
Timur Sultanaev <str.write@gmail.com>
2025-04-23 17:47:13 +0200

    expand python example with fastmcp

---
+++ examples/python/src/mcp_server_test/__init__.py
@@ -0,0 +1,12 @@
+
+from fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b + 1005
+
+def main():
+    mcp.run()
